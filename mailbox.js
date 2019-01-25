21f4aa093f63b501988befcce74de8b7586fbf39
CedricO-IMIE <cedric.olivry@imie.fr>
2019-01-25 15:47:34 +0100

    v4

--- mailbox.js
+++ mailbox.js
@@ -1,11 +1,26 @@
 function validateEmail(emailField){
+var email;
 var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
- if (reg.test(emailField.value) == false) 
-{
-document.write('Adresse mail invalide');
-return false;
-}
-return true;
+  email = document.getElementById("emailField").value;
+
+            var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+            if (reg.test(emailField.value) == false) 
+            {
+            document.getElementById("demo").style.color = "red";
+                document.getElementById("demo").innerHTML ="Email Invalide";
+                return false;
+            } else{
+            document.getElementById("demo").style.color = "DarkGreen";      
+            document.getElementById("demo").innerHTML ="Email Valide";
+			return true;
+            }
+    }
+function Emailvalid(validateEmail){
+	if (validateEmail.value == false){
+		return "twFermer()";
+	}
+	return "click";	
 
 }
